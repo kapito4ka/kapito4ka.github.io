ed9acee98dbb69d42066779e5d4e27991155eed8
kapito4ka <59176703+kapito4ka@users.noreply.github.com>
2023-12-18 20:45:27 +0300

    Add files via upload

---
+++ gigasite/javas.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+document.querySelector('.open-popup').addEventListener('click', openPopup);
